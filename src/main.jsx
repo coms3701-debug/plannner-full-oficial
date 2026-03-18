5a69811ecb701fafb44a3669baf723cff1b581a2
coms3701-debug <coms3701@gmail.com>
2026-03-18 15:37:06 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <-- GARANTA QUE ESTA LINHA EXISTE
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
